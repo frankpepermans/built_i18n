ac865ac943dd8bd61c3b30c11b398cce923963a2
Igindo\FrankPepermans <frank@igindo.com>
2019-10-07 21:25:10 +0200

    no message

--- lib/src/codegen/i18n_generator.dart
+++ lib/src/codegen/i18n_generator.dart
@@ -11,9 +11,9 @@ class I18nGenerator extends Generator {
 
   @override
   FutureOr<String> generate(LibraryReader library, BuildStep buildStep) async {
+    final path = buildStep.inputId.path.split('/').sublist(1).join('/');
     final ClassElement element = library.allElements.firstWhere(
-        (Element element) =>
-            element.location.components.first.contains(buildStep.inputId.path),
+        (Element element) => element.location.components.first.contains(path),
         orElse: () => null) as ClassElement;
 
     if (element == null) return null;
